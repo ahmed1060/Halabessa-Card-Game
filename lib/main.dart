b090353dcf52e42a834ad7292a9a3786c636008a
ahmed1060 <ahmed.hossam1060@gmail.com>
2024-05-02 15:49:20 +0300

    first code

--- lib/main.dart
+++ lib/main.dart
@@ -6,120 +6,127 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        appBar: AppBar(title: Text('Halabessa')),
+        body: CardStack(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class CardStack extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _CardStackState createState() => _CardStackState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CardStackState extends State<CardStack> {
+  final List<CardInfo> cards = [
+    CardInfo(value: 'K', suit: Suit.hearts),
+    CardInfo(value: 'A', suit: Suit.hearts),
+    CardInfo(value: 'J', suit: Suit.hearts),
+    CardInfo(value: '7', suit: Suit.hearts),
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  double _baseLeftOffset(int index, double cardWidth, double cardSpacing, double sizeWidth) {
+    final totalCardWidth = cardWidth * cards.length;
+    final totalSpacingWidth = cardSpacing * (cards.length - 1);
+    final totalWidth = totalCardWidth + totalSpacingWidth;
+    return (sizeWidth - totalWidth) / 2 + index * (cardWidth + cardSpacing);
+  }
+
+  double _baseTopOffset(double cardWidth, double sizeHeight) {
+    return sizeHeight * 0.75 - cardWidth * 0.7;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    var size = MediaQuery.of(context).size;
+    final cardWidth = size.width * 0.07;
+    final cardSpacing = size.width * 0.01;
+    final cardHeight = cardWidth * 1.4;
+
+    return Stack(
+      children: List.generate(cards.length, (index) {
+        final baseLeftOffset = _baseLeftOffset(index, cardWidth, cardSpacing, size.width);
+        final baseTopOffset = _baseTopOffset(cardWidth, size.height);
+
+        return Positioned(
+          left: baseLeftOffset,
+          top: baseTopOffset,
+          child: CardWidget(
+            card: cards[index],
+            onTap: () => _handleCardTap(cards[index]),
+          ),
+        );
+      }),
     );
   }
+
+  void _handleCardTap(CardInfo card) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Card Tapped"),
+          content: Text("You tapped on ${card.value} of ${card.suit}."),
+          actions: <Widget>[
+            TextButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+enum Suit { hearts, diamonds, clubs, spades }
+
+class CardInfo {
+  final String value;
+  final Suit suit;
+
+  CardInfo({required this.value, required this.suit});
+}
+
+class CardWidget extends StatelessWidget {
+  final CardInfo card;
+  final VoidCallback onTap;
+
+  const CardWidget({required this.card, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    final cardWidth = MediaQuery.of(context).size.width * 0.07;
+    final cardHeight = cardWidth * 1.4;
+
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        width: cardWidth,
+        height: cardHeight,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.black),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(card.value, style: TextStyle(fontSize: cardWidth * 0.2)),
+              SizedBox(height: 8),
+              Text(card.suit.name, style: TextStyle(fontSize: cardWidth * 0.15)),
+            ],
+          ),
+        ),
+),
+);
+}
 }
