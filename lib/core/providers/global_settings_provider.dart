8c38e49fa411d49e570e1d66c2cfc2c6c9a466d1
ahmed1060 <ahmed.hossam1060@gmail.com>
2026-03-02 02:43:53 +0200

    bug fix

--- lib/core/providers/global_settings_provider.dart
+++ lib/core/providers/global_settings_provider.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/global_settings.dart';
 
